31305324880687b8e536cc81ff1a97773e7482e4
kevinqiyefa <kevinqiyf@hotmail.com>
2018-03-17 16:33:16 -0700

    p17

---
+++ 17-head.js
@@ -0,0 +1,17 @@
+// Write a function called head which returns the first element of the input array.
+
+// Examples:
+
+// head([1, 2, 3]) // 1 
+// head([
+//   {name: "Elie"},
+//   {name: "Matt"},
+//   {name: "Michael"}
+// ]); // {name: "Elie"}
+// head([]) // undefined
+
+
+function head(arr){
+  // add whatever parameters you deem necessary - good luck
+  return arr[0];
+}
