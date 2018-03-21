61c42d824254354b68bd79581fc8bcde05dc48bc
kevinqiyefa <kevinqiyf@hotmail.com>
2018-03-21 00:00:41 -0700

    p42

---
+++ 42-union.js
@@ -0,0 +1,16 @@
+// Write a function called union which accepts a variable number of arguments, which are all arrays. The function returns a new array of unique values from all the parameters.
+
+// Examples:
+
+// union([2],[1, 2]); // [2, 1]
+// union([2],[1, 2], [3], [3, 4, 5]); // [2,1,3,4,5]
+// union([2],[1, 2], [6,8], [4,5,1,2], [2,7,6,9]); // [2,1,6,8,4,5,7,9]
+
+
+function union(...arrs){
+  // add whatever parameters you deem necessary - good luck!
+  
+  let arr = [].concat(...arrs);
+  
+  return [...new Set(arr)];
+}
