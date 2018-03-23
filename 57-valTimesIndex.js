14e0e0ab280cc008b4e58e2e1eecfa6664422d30
kevinqiyefa <kevinqiyf@hotmail.com>
2018-03-22 23:05:43 -0700

    p57

---
+++ 57-valTimesIndex.js
@@ -0,0 +1,16 @@
+// Write a function called valTimesIndex which accepts an array and returns a new array with each value multiplied by its index in the array.
+
+// Examples:
+
+// valTimesIndex([1,2,3]) // [0,2,6]
+// valTimesIndex([1,-2,-3]) // [0,-2,-6]
+// valTimesIndex([4,2,8,0,1]) // [0, 2, 16, 0, 4]
+// valTimesIndex([]) // []
+
+// Try to solve this using map.
+
+
+function valTimesIndex(arr){
+  // add whatever parameters you deem necessary - good luck!
+  return arr.map((x,i)=>x*i);
+}
