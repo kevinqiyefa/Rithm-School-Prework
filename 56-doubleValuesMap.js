7487294f946b1969d4553a6b2872650f6c7a7873
kevinqiyefa <kevinqiyf@hotmail.com>
2018-03-22 23:02:36 -0700

    p56

---
+++ 56-doubleValuesMap.js
@@ -0,0 +1,15 @@
+// Write a function called doubleValuesMap which accepts an array and returns a new array with all the values in the array passed to the function doubled.
+
+// Examples:
+
+// doubleValuesMap([1,2,3]); // [2,4,6]
+// doubleValuesMap([1,-2,-3]); // [2,-4,-6]
+
+// Try to solve this using map.
+
+
+function doubleValuesMap(arr){
+  // add whatever parameters you deem necessary - good luck!
+  
+  return arr.map(x=>x*2);
+}
